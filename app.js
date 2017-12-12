cda354b97faaf49086698f4bbeecfb364d116b5d
rococolate <chocolatebloom@gmail.com>
2017-12-12 14:52:05 +0800

    not use db

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/apod')
+// mongoose.connect('mongodb://localhost/apod')
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var router = express.Router();
 
 const request = require('request');
 let dateFormat = require('../lib/dateFormat')
-let ApodModel = require('../models/apodModel')
-let saveApodToDB = require('../lib/saveApodToDB')
+// let ApodModel = require('../models/apodModel')
+// let saveApodToDB = require('../lib/saveApodToDB')
 let lastDay = require('../lib/lastDay')
 
 
@@ -18,7 +18,7 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.get('/apod', function(req, res, next) {
-    console.log(req.query)
+    // console.log(req.query)
     let reqDate = ''
 
     if (req.query.last) {
@@ -30,42 +30,42 @@ router.get('/apod', function(req, res, next) {
     }
 
     let url = `https://api.nasa.gov/planetary/apod?hd=true&api_key=1kD6jUfLLv5X8BYHfExJ5e6trif3mCT7pOVnUX4e${reqDate != '' ? '&date=' + reqDate : '' }`
-    console.log(url)
+    // console.log(url)
 
     let findDate = reqDate == "" ? dateFormat(new Date()) : reqDate
-    console.log('findDate:', findDate)
+    // console.log('findDate:', findDate)
 
-    getApodFormBD(findDate, function(apod) {
-        res.json({
-            code: '0000',
-            msg: 'success',
-            data: apod
-        })
-    }, function() {
+    // getApodFormBD(findDate, function(apod) {
+    //     res.json({
+    //         code: '0000',
+    //         msg: 'success',
+    //         data: apod
+    //     })
+    // }, function() {
         getApodFormNASA(url)
 
-    })
+    // })
 
 
-    function getApodFormBD(findDate, successCallback, failCallback) {
-        ApodModel.findByDate(findDate, function(err, apod) {
-            if (err) {
-                console.log(err)
-                return err
-            }
-            console.log('apod=====>', apod)
-            if (apod != null) {
-                if (typeof successCallback == 'function') {
-                    successCallback(apod)
-                }
+    // function getApodFormBD(findDate, successCallback, failCallback) {
+    //     ApodModel.findByDate(findDate, function(err, apod) {
+    //         if (err) {
+    //             console.log(err)
+    //             return err
+    //         }
+    //         console.log('apod=====>', apod)
+    //         if (apod != null) {
+    //             if (typeof successCallback == 'function') {
+    //                 successCallback(apod)
+    //             }
 
-            } else {
-                if (typeof failCallback == 'function') {
-                    failCallback()
-                }
-            }
-        })
-    }
+    //         } else {
+    //             if (typeof failCallback == 'function') {
+    //                 failCallback()
+    //             }
+    //         }
+    //     })
+    // }
 
 
 
@@ -74,20 +74,20 @@ router.get('/apod', function(req, res, next) {
         let returnData = {}
         try {
             request(url, (error, response, body) => {
-                console.log('error:', error, 'body', body)
-                console.log('================')
-                console.log(response.headers)
-                console.log('================')
-                console.log(Number(response.headers['x-ratelimit-remaining']))
-                console.log('================')
-                console.log('response.statusCode', response.statusCode)
-                console.log('================')
+                // console.log('error:', error, 'body', body)
+                // console.log('================')
+                // console.log(response.headers)
+                // console.log('================')
+                // console.log(Number(response.headers['x-ratelimit-remaining']))
+                // console.log('================')
+                // console.log('response.statusCode', response.statusCode)
+                // console.log('================')
 
                 if (!error && response.statusCode == 200) {
 
                     let data = JSON.parse(body)
 
-                    saveApodToDB(findDate, data)
+                    // saveApodToDB(findDate, data)
 
                     returnData = {
                         code: '0000',
@@ -106,7 +106,7 @@ router.get('/apod', function(req, res, next) {
                 }
             })
         } catch (e) {
-            console.log(e)
+            // console.log(e)
             returnData = {
                 code: '1000',
                 msg: 'error',
